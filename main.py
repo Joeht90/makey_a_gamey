8b4cd1fff6bb64f8f4e6a7ec9d8d984765f4c7da
joeht90 <joeht90@gmail.com>
2025-07-06 09:00:03 -0700

    started implementing the new scenes.py file into main.py need to work out readability of code

--- main.py
+++ main.py
@@ -1,35 +1,30 @@
 from scene import Scene
 from scene_connections import SceneConn
+import scenes
 
-connect = SceneConn()
-room = Scene("room", "first room in the game")
-connect.add_connection("room", north="room_2")
-room_2 = Scene("room_2", "second room in the game")
-room_3 = Scene("room_3", "third room in the game")
 
 def main():
     print("Welcome to new game!")
-    current_room = room
+    current_room = scenes.room_1
     while True:
         command = input("Would you like to [q]uit or need some [h]elp?")
         if command == 'q':
             print("Quitting game...")
             break
-        if command == 'go to room_2':
-            current_room = room_2
-            print("You are now in room 2.")
-            continue
         if command == 'c':
             current_room.print_name()
             continue
         if command == 'h':
             print("this is where the help screen would appear, if I had one.")
         if command == 'north':
-            current_room = connect.move_room(current_room.name, "north")
+            # TODO: clean up the scenes.connect.move_room, that is a lot of code
+            # we do not need but it works for now
+            current_room = scenes.connect.move_room(current_room.name, "north")
             print(current_room)
             continue
         else:
             print("that was not a valid option.")
+
 
 if __name__ == "__main__":
     main() 
---
+++ scenes.py
@@ -0,0 +1,15 @@
+from scene import Scene 
+from scene_connections import SceneConn
+
+# Create all the scenes you need for your game here
+room_1 = Scene("room_1", "starting room")
+room_2 = Scene("room_2", "middle room with combat")
+room_3 = Scene("room_3", "last room with chest")
+
+# SceneConnections instance do not alter the line below
+connect = SceneConn()
+
+# Create connections to all of the above scenes
+connect.add_connection(room_1.name, north=room_2.name)
+connect.add_connection(room_2.name, east=room_3.name, south=room_1.name)
+connect.add_connection(room_3.name, west=room_2.name)
