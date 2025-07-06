08aeac775c5398069409970243468fc65086b71b
joeht90 <joeht90@icloud.com>
2025-07-06 16:12:22 -0700

    added error handling for go inputs that dont have a proper direction

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from scene import Scene
 from scene_connections import SceneConn
-import scenes
-
+import scenes as s
+scenes = s.connect
 
 def main():
     print("Welcome to new game!")
-    location = scenes.room_1
+    location = s.room_1
     while True:
         command = input("Would you like to [q]uit or need some [h]elp?")
         if command == 'q':
@@ -18,10 +18,13 @@ def main():
             print("this is where the help screen would appear, if I had one.")
         if 'go' in command:
             direction = command.split()
-            if scenes.connect.connections[location.name][direction[-1]] == None:
-                print("You cannot go that direction!")
-                continue
-            location = scenes.connect.move_room(location.name, direction[-1])
+            try:
+                if scenes.connections[location.name][direction[-1]] == None:
+                    print("You cannot go that direction!")
+                    continue
+                location = scenes.move_room(location.name, direction[-1])
+            except KeyError:
+                print("I'm not sure what you are asking. Try again.")
             continue
         else:
             print("that was not a valid option.")
