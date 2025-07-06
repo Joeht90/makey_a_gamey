41ae6074e65f769864c292bd49cb25ab5e51f027
joeht90 <joeht90@icloud.com>
2025-07-06 16:05:25 -0700

    Refined the movement if statements in main.py to look for the verb go then check if the direction = None before commiting to the move

--- main.py
+++ main.py
@@ -4,42 +4,24 @@ import scenes
 
 
 def main():
-    print(scenes.connect.connections)
     print("Welcome to new game!")
-    current_room = scenes.room_1
+    location = scenes.room_1
     while True:
         command = input("Would you like to [q]uit or need some [h]elp?")
         if command == 'q':
             print("Quitting game...")
             break
         if command == 'c':
-            current_room.print_name()
+            location.print_name()
             continue
         if command == 'h':
             print("this is where the help screen would appear, if I had one.")
-        if  'north' in command:
-            # TODO: clean up the scenes.connect.move_room, that is a lot of code
-            # we do not need but it works for now
-            current_room = scenes.connect.move_room(current_room.name, "north")
-            print(current_room.name)
-            continue
-        if  'south' in command:
-            # TODO: clean up the scenes.connect.move_room, that is a lot of code
-            # we do not need but it works for now
-            current_room = scenes.connect.move_room(current_room.name, "south")
-            print(current_room.name)
-            continue
-        if  'west' in command:
-            # TODO: clean up the scenes.connect.move_room, that is a lot of code
-            # we do not need but it works for now
-            current_room = scenes.connect.move_room(current_room.name, "west")
-            print(current_room.name)
-            continue
-        if  'east' in command:
-            # TODO: clean up the scenes.connect.move_room, that is a lot of code
-            # we do not need but it works for now
-            current_room = scenes.connect.move_room(current_room.name, "east")
-            print(current_room.name)
+        if 'go' in command:
+            direction = command.split()
+            if scenes.connect.connections[location.name][direction[-1]] == None:
+                print("You cannot go that direction!")
+                continue
+            location = scenes.connect.move_room(location.name, direction[-1])
             continue
         else:
             print("that was not a valid option.")
