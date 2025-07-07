4f5d86977e083fd0e661f9d46f45d6f5a42c7dfb
joeht90 <joeht90@gmail.com>
2025-07-07 05:08:10 -0700

    just cleanned up the column length of the add_connection method

--- scene_connections.py
+++ scene_connections.py
@@ -2,11 +2,12 @@ class SceneConn:
     def __init__(self):
         self.connections = {}
 
-    def add_connection(self, current_scene, north=None, south=None, east=None, west=None):
+    def add_connection(self, current_scene, north=None, south=None,
+                       east=None, west=None):
         self.connections[current_scene] = {"north": north, "south": south,
                                            "east": east, "west": west}
 
     def move_room(self, current_scene, direction):
         return self.connections[current_scene][direction]
 
-  
+
