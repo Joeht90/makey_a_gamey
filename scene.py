3916a2e39ea056ee6fb00bfd3b99bfc2709b8122
joeht90 <joeht90@gmail.com>
2025-07-07 04:59:57 -0700

    scene.py modified to start inheriting from a gameobject class, cleans the code up a bit

--- scene.py
+++ scene.py
@@ -1,12 +1,21 @@
 from inventory import Inventory
 
-class Scene:
+
+class GameObject:
     def __init__(self, name, description):
         self.name = name
         self.description = description
-        self.inventory = Inventory()
 
     def print_name(self):
         print(self.name)
 
 
+class Scene(GameObject):
+    def __init__(self, name, description):
+        super().__init__(name, description)
+        self.inventory = Inventory()
+
+
+class Item:
+    def __init__(self, name, description):
+        super().__init__(name, description)
