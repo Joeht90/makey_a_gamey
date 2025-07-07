402c174895fe081830e416a98c5e753f9f721aa6
joeht90 <joeht90@icloud.com>
2025-07-06 18:32:25 -0700

    started working on inventory and items

--- scene.py
+++ scene.py
@@ -2,7 +2,7 @@ class Scene:
     def __init__(self, name, description):
         self.name = name
         self.description = description
-
+        self.inventory = []
 
     def print_name(self):
         print(self.name)
