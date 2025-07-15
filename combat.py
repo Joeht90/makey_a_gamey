95b7b1a01197f1f5226204dfddd7de9da49c7c08
joeht90 <joeht90@gmail.com>
2025-07-14 20:34:34 -0700

    Wrote out todo list for combat functions

---
+++ combat.py
@@ -0,0 +1,5 @@
+# TODO: Create a loop for combat
+# TODO: Take in input for commands
+# TODO: Make a func for decrementing health
+# TODO: Make a func for transferring inventory from enemy to player
+# TODO: Create win/lose condition
